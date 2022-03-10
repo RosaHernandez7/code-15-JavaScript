acf36ed88c7ebc946891d07f946b7455b3a01b9c
Rosa <rhg1811@gmail.com>
2022-03-09 22:37:28 -0600

    Challenges with JS

---
+++ 0_intro/js/arrays.js
@@ -0,0 +1,5 @@
+//Todos los elementos qu eno sean primitivos  vienen de object 
+//Array es un objeto
+//Lista de ele,mntos que puedo manipular 
+var alumnos = ["alumnos", "jorge","diego"],
+ 
--- 0_intro/js/main.js
+++ 0_intro/js/main.js
@@ -1,2 +1,26 @@
 alert("Hola mundo!");
 console.log("que onda!?");
+
+let edad = parseInt(prompt("Ingresa tu edad"))
+if (edad<=4)
+{
+    prompt("Muy koven")
+}
+    
+    else if(edad>4 && edad<=99)
+    {
+        prompt(Diviertete)
+    }
+        
+        else
+       {
+          prompt(nLo siento, pero para el dominio no hay límite) 
+       } 
+
+
+animal = parseInt(prompt("Ingresa el animal"))
+
+switch (animal){
+case "lobo": alert("auh")
+
+}
